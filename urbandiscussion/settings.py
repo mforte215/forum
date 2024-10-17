7e822f603dc6cd82b8af87a2886f6edf91ff1217
mforte215 <mark.fortee@gmail.com>
2024-10-17 13:23:25 -0400

    another

--- urbandiscussion/settings.py
+++ urbandiscussion/settings.py
@@ -14,8 +14,7 @@ SECRET_KEY = "lhkfsfwoe9f88fohielthtohw49t04t9wt4)R)*r88rw888r8rhewr38r38twtpw9e
 IS_HEROKU_APP = "DYNO" in os.environ and not "CI" in os.environ
 
 # SECURITY WARNING: don't run with debug turned on in production!
-if not IS_HEROKU_APP:
-    DEBUG = True
+DEBUG=True
 
 # On Heroku, it's safe to use a wildcard for `ALLOWED_HOSTS``, since the Heroku router performs
 # validation of the Host header in the incoming HTTP request. On other platforms you may need to
