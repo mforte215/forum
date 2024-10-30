8923c0c10cad161ca845e4aef81b137044020230
mforte215 <mark.fortee@gmail.com>
2024-10-30 09:22:43 -0400

    rich text box

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from ckeditor.fields import RichTextField
 import uuid
 
 
@@ -20,6 +21,6 @@ class Article(models.Model):
     published_date = models.DateTimeField(auto_now_add=True, editable=False)
     image = models.ImageField(upload_to='images/', null=True)
     snippet = models.CharField(max_length=250)
-    content = models.TextField(null=True)
+    main_content = RichTextField(default='')
     def __str__(self):
         return self.title
--- urbandiscussion/settings.py
+++ urbandiscussion/settings.py
@@ -1,6 +1,5 @@
 from pathlib import Path
 import os
-import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -29,6 +28,7 @@ else:
 # Application definition
 
 INSTALLED_APPS = [
+    'ckeditor',
     "whitenoise.runserver_nostatic",
     "app",
     "django.contrib.admin",
