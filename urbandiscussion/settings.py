b5f77519721b30048540e99668553de63e198ae5
mforte215 <mark.fortee@gmail.com>
2025-01-03 15:47:02 -0500

    another commit for static

--- urbandiscussion/settings.py
+++ urbandiscussion/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "storages",
 ]
 
 MIDDLEWARE = [
@@ -121,9 +122,28 @@ USE_TZ = True
 
 WHITENOISE_KEEP_ONLY_HASHED_FILES = True
 
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+STORAGES = {
+    "default": {
+        "BACKEND": "storages.backends.s3.S3Storage"
+    },
+}
+
+STATICFILES_STORAGE = "storages.backends.s3.S3Storage"
+
+AWS_ACCESS_KEY_ID = os.environ.get('ACCESS_KEY_ID', None)
+AWS_SECRET_ACCESS_KEY = os.environ.get('SECRET_KEY_ID', None)
+AWS_STORAGE_BUCKET_NAME = os.environ.get('BUCKET_ID', None)
+
+AWS_DEFAULT_ACL = 'public-read'
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
+AWS_LOCATION = 'static'
+STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-STATIC_ROOT = BASE_DIR / "staticfiles"
-STATIC_URL = "static/"
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
