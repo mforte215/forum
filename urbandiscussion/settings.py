76c3492a4c1f1d5d221b015f3bc5e23256e70c43
mforte215 <mark.fortee@gmail.com>
2025-01-03 15:15:24 -0500

    static file updates

--- urbandiscussion/settings.py
+++ urbandiscussion/settings.py
@@ -125,5 +125,5 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATIC_URL = "static/"
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
