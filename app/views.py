6e9d018cce029afac0db248c878449dc00ad90b0
mforte215 <mark.fortee@gmail.com>
2025-01-06 14:22:26 -0500

    dev update

--- app/views.py
+++ app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.http import Http404
 from .models import Article
 def index(request):
     latest_article_list = Article.objects.order_by("-published_date")
@@ -9,3 +10,13 @@ def articleDetail(request, slug):
     article = get_object_or_404(Article, slug=slug)
     context = {"article": article}
     return render(request, "app/article.html", context)
+
+def articleListView(request):
+    try:
+        articles = Article.objects.all()[:5]
+        context = {"articles": articles}
+        return render(request, "app/articles.html", context)
+    except:
+        raise Http404("No articles found!")
+
+
